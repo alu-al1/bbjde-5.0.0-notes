c97bb66fe2928bfee62a65179e8ae494da0e1f9f
andrej povarov <a.povarov@atg-soft.ru>
2025-02-28 03:48:12 +0300

    fixing fifo; support for direct stdin entries

--- run_wine_bb_sim_9700_fifo_interactive.sh
+++ run_wine_bb_sim_9700_fifo_interactive.sh
@@ -13,23 +13,34 @@ sessionname=$2
 
 ## TODO Usage as a separate routine
 
-## TODO rewrite to `utils.check_is_set_or_die_critical"
-[ -z "$fifopipe" ] && echo -en "Arg error: No fifo pipe given.\nUsage $0 fifopipe [session=first_session_found]\n" && exit 1 || echo
-[ -p "$fifopipe" ] || mkfifo "$fifopipe"
-[ -p "$fifopipe" ] || { echo -en "error: Failed to ensure fifo pipe $fifopipe.\n"; exit 1; }
-
 export WINEPREFIX="$DIR"/wine
 
 if [ -z "$sessionname" ]; then
-	sessionname=$(wine cmd /c "fledgecontroller.exe /get-sessions" | head -n1)
+    sessionname=$(wine cmd /c "fledgecontroller.exe /get-sessions" | head -n1 | tr -d '\r')
 fi
 
 [ -z "$sessionname" ] && echo -en "Arg error: No session name given and no active sessions found.\nUsage $0 fifopipe [session=first_session_found]\n" && exit 1 || echo
 
 
+## TODO rewrite to `utils.check_is_set_or_die_critical"
+## TODO interactive mode
+directmode=0
+if [ -z "$fifopipe" ];then
+    echo -en "No fifopipe is given, switching to direct mode.\n\n"
+    directmode=1
+else
+    [ -p "$fifopipe" ] || mkfifo "$fifopipe"
+    [ -p "$fifopipe" ] || { echo -en "error: Failed to ensure fifo pipe $fifopipe.\n"; exit 1; }
+fi
+
 wine cmd /c "echo %PATH% & fledgecontroller.exe /help"
 wine_cmd_c="fledgecontroller.exe /session=$sessionname"
 
-# or stdbuf -i0 some_command < my_pipe
-# or exec 3<> my_pipe && some_command <&3
-socat -u PIPE:"$fifopipe" EXEC:'wine cmd /c '"$wine_cmd_c"
+
+if [ "$directmode" -ne 0 ]; then
+    wine cmd /c "$wine_cmd_c" <&1
+else
+    # socat -u PIPE:"$fifopipe" EXEC:'wine cmd /c '"$wine_cmd_c"	## Issue: not working for me for now
+    # stdbuf -i0 wine cmd /c "$wine_cmd_c" < "$fifopipe"			## Issue: oneshot and then closes
+    exec 3<> "$fifopipe" && wine cmd /c "$wine_cmd_c" <&3
+fi
