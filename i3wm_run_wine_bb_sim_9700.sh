a837b1ee1755f2a0174ddeb4ffe190463f722732
andrej povarov <a.povarov@atg-soft.ru>
2025-03-01 03:14:42 +0300

    +TODOs

--- i3wm_run_wine_bb_sim_9700.sh
+++ i3wm_run_wine_bb_sim_9700.sh
@@ -11,6 +11,7 @@ source "$DIR"/i3wm_utils.sh
 ## use xprop on the window to get its attributes
 window_name="BlackBerry 9700 Simulator"
 
+## TODO get rid of sleep and make it more event-based-ish
 { sleep 2; i3wm_place_window "$window_name" 8 2; } \
 & \
 "$DIR"/run_wine_bb_sim_9700.sh
